da1d50c509f802a9b6a74737b274f4a179c3521e
NagarajDoddamani <doddamaninagaraj5@gmail.com>
2026-03-22 20:38:36 +0530

    The College Boundry restriction

---
+++ src/config/mapBounds.js
@@ -0,0 +1,11 @@
+export const campusCenter = {
+  lat: 15.430830317583812,
+  lng: 75.014605914028,
+};
+
+export const campusBounds = {
+  north: 15.432013358737056,
+  south: 15.427705623412587,
+  west: 75.01195223907536,
+  east: 75.01750129750481,
+};
